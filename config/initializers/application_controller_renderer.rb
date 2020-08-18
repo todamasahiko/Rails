7ca23ee1c5a29a0ba431c20035b7213901f2f27b
todamasahiko <todaamasahiko@yahoo.co.jp>
2020-08-18 09:44:28 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'bdca257d3a2c47ddea026c400c039b72ec108acb5ee69de749b3c1b0d5a6a518909e56d5ef3ade1e97275aac79d6083bfdc5877b9e2aa816ca8ea6156e075817'
